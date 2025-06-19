0ca714cbd1afd10e8ef6ce3c7c70f58863ce88a7
“parn” <titinanjaem@gmail.com>
2025-06-19 10:59:48 +0700

    add toggle_complete route for quests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :quests
   root "quests#index"
+  resources :quests do
+    member do
+      patch :toggle_complete
+    end
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
